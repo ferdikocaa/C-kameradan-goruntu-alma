3946c50e026457315231f1c48dddc55b5990efe5
Ferdi KOCA <ferdikoca29@gmail.com>
2018-03-13 00:01:05 +0300

    Kameradan Goruntu Alma
    C# ile kameradan goruntu alma işlemi tamamlandı.Goruntu işleme adımlarına başlanacak.

---
+++ GoruntuIsleme-ferdikocaa/GoruntuIsleme-ferdikocaa/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace GoruntuIsleme_ferdikocaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webCamCapture1 = new WebCam_Capture.WebCamCapture();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webCamCapture1
+            // 
+            this.webCamCapture1.CaptureHeight = 240;
+            this.webCamCapture1.CaptureWidth = 320;
+            this.webCamCapture1.FrameNumber = ((ulong)(0ul));
+            this.webCamCapture1.Location = new System.Drawing.Point(0, 0);
+            this.webCamCapture1.Name = "WebCamCapture";
+            this.webCamCapture1.Size = new System.Drawing.Size(342, 252);
+            this.webCamCapture1.TabIndex = 0;
+            this.webCamCapture1.TimeToCapture_milliseconds = 100;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 260);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(256, 281);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 318);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kameradan Goruntuyu Kontrol Etme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WebCam_Capture.WebCamCapture webCamCapture1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ GoruntuIsleme-ferdikocaa/GoruntuIsleme-ferdikocaa/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace GoruntuIsleme_ferdikocaa
+{
+    public partial class Form1 : Form
+    {
+         public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection webcam;
+        private VideoCaptureDevice cam;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString);
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+            cam.Start();
+        }
+        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();//picture box'a resmi atıyoruz.
+            pictureBox1.Image = bmp;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (cam.IsRunning)
+            {
+                cam.Stop(); 
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo item in webcam)
+            {
+                comboBox1.Items.Add(item.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog swf = new SaveFileDialog();
+            swf.Filter = "(*.jpg)|*.jpg|Bitma*p(*.bmp)|*.bmp";
+            DialogResult dialog = swf.ShowDialog();
+            if (dialog == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(swf.FileName);
+            }
+        }
+    }
+}
